81357f44ac7f779c7d7d501585086a53f23e36e9
zm <1zaur1@mail.ru>
2018-10-08 10:42:19 +0300

    0126 - страница "Калькулятор": сделал показ и скрытие блоков под флагами

--- 0004/src/js/main_form_calc.js
+++ 0004/src/js/main_form_calc.js
@@ -22,14 +22,58 @@ function ready() {
     check3div = document.getElementById('check3div');
     check3img = document.getElementById('check-img3');
 
+    let divNegabarit = document.getElementById('negabarit');
+    let divSupportSend = document.getElementById('supportSend');
+    let divSupportAccept = document.getElementById('supportAccept');
+
+
+
+    //*******************************************************************
+    //флаг - 01 -отключаем флаг негабаритного груза
+    let check1 = document.getElementById('check1');
+    check1.checked = false;
+    check1img.style.display = 'none';
+    showHideDivFromFlag(divNegabarit, false);
+    //console.log(check1.checked);
+
+
+
+    //*******************************************************************
+    //флаг - 02 -отключаем флаг сопровождение в пункте отправки
+    let check2 = document.getElementById('check2');
+    check2.checked = false;
+    check2img.style.display = 'none';
+    showHideDivFromFlag(divSupportSend, false);
+    //console.log(check1.checked);
+
+
+    //*******************************************************************
+    //флаг - 03 -отключаем флаг сопровождение в пункте приемки
+    let check3 = document.getElementById('check3');
+    check3.checked = false;
+    check3img.style.display = 'none';
+    showHideDivFromFlag(divSupportAccept, false);
+    //console.log(check1.checked);
+
+
+    function showHideDivFromFlag(dv, bool) {
+        if (bool){
+            dv.style.display = 'block';
+        }else {
+            dv.style.display = 'none';
+        }
+    }
+
+
     document.getElementById('check1').onclick = function () {
         if (this.checked){
             //alert(this.value);
             check1div.style.backgroundColor = "#222222";
             check1img.style.display = 'flex';
+            showHideDivFromFlag(divNegabarit, true);
         }else {
             check1img.style.display = 'none';
-
+            showHideDivFromFlag(divNegabarit, false);
         }
     }
 
@@ -38,9 +82,10 @@ function ready() {
             //alert(this.value);
             check2div.style.backgroundColor = "#222222";
             check2img.style.display = 'flex';
+            showHideDivFromFlag(divSupportSend, true);
         }else {
             check2img.style.display = 'none';
-
+            showHideDivFromFlag(divSupportSend, false);
         }
     }
 
@@ -49,8 +94,10 @@ function ready() {
             //alert(this.value);
             check3div.style.backgroundColor = "#222222";
             check3img.style.display = 'flex';
+            showHideDivFromFlag(divSupportAccept, true);
         }else {
             check3img.style.display = 'none';
+            showHideDivFromFlag(divSupportAccept, false);
 
         }
     }
